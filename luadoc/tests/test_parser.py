3fa2223bca521b82f4399ed52eba9545509d311d
boolangery <eliott.dumeix@gmail.com>
2019-01-26 20:01:46 +0100

    create some tests.

--- luadoc/tests/test_parser.py
+++ luadoc/tests/test_parser.py
@@ -1,12 +1,32 @@
+import os
 import unittest
 from luadoc.parser import DocParser
-from luadoc.printers import toPrettyStr
+from luadoc.printers import to_pretty_json
 
 
 class ParserTestCase(unittest.TestCase):
-    def test_parser(self):
-        src = r'''
-            '''
-        model = DocParser().build_module_doc_model(src)
-        print(toPrettyStr(model))
+    CURRENT_DIR: str = os.path.dirname(__file__)
+    SOURCE_ROOT: str = os.path.join(CURRENT_DIR, "source")
+    LUA_EXT: str = ".lua"
+    JSON_EXT: str = ".json"
 
+    def make_test_from_sources(self, test_name: str):
+        lua_file = open(os.path.join(ParserTestCase.SOURCE_ROOT, test_name + ParserTestCase.LUA_EXT), 'r')
+        tree_file = open(os.path.join(ParserTestCase.SOURCE_ROOT, test_name + ParserTestCase.JSON_EXT), 'r')
+
+        lua_source = lua_file.read()
+        exp_doc_tree = tree_file.read()
+
+        module = DocParser().build_module_doc_model(lua_source)
+        json_doc_tree = to_pretty_json(module)
+
+        lua_file.close()
+        tree_file.close()
+
+        self.assertEqual(json_doc_tree, exp_doc_tree)
+
+    def test_class(self):
+        self.make_test_from_sources("class")
+
+    def test_class_module(self):
+        self.make_test_from_sources("class_module")
